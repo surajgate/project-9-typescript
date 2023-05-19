0505fb7dbcd96b2336efd7b8d6c293c50257a3c8
suraj gate <surajgate73@gmail.com>
2023-05-19 14:09:30 +0530

    UI Changes

--- src/components/SingleColor.tsx
+++ src/components/SingleColor.tsx
@@ -1,5 +1,6 @@
-import { Container, Typography } from "@mui/material";
-import { useState } from "react";
+import { Box, Container, Typography } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import Clipboard from "clipboard";
 
 type ColorProps = {
   rgb: number[];
@@ -12,19 +13,36 @@ const SingleColor = ({ rgb, weight, index, hexColor }: ColorProps) => {
   const [alert, setAlert] = useState<boolean>(false);
   const bcg = rgb.join(",");
 
+   const handleCopy = () => {
+     const clipboard = new Clipboard(".copy", {
+       text: () => '#'+hexColor,
+     });
+     clipboard.on("success", () => {
+       clipboard.destroy();
+     });
+     clipboard.on("error", () => {
+       clipboard.destroy();
+     });
+    //  clipboard.onClick({ target: { className: "copy" } });
+   };
+
   return (
     <>
       <Container
         sx={{
           backgroundColor: `rgb(${bcg})`,
           height: "10rem",
-          color : `${index > 10 ? 'white' : 'black'}`
+          color: `${index > 10 ? "white" : "black"}`,
         }}
       >
         <Typography sx={{ alignItems: "center", p: 2 }} paragraph>
           {weight}%
         </Typography>
-        <Typography paragraph>#{hexColor}</Typography>
+        <Box onClick={handleCopy}>
+          <Typography className="copy" paragraph>
+            #{hexColor}
+          </Typography>
+        </Box>
       </Container>
     </>
   );
